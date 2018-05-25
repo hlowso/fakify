a61bde654a4691f7aba6d7171ebee4d1313ed411
hlowso <h.canderleigh@gmail.com>
2018-05-25 18:35:10 -0400

    commiting changes after clearing cache

--- src/midi/loadSoundfonts.js
+++
@@ -1,9 +0,0 @@
-import loadAcousticGrandPiano from "./soundfonts/acoustic_grand_piano-ogg";
-import loadSynthDrum from "./soundfonts/synth_drum-ogg";
-
-const loadSoundfonts = MIDI => {
-    loadAcousticGrandPiano(MIDI);
-    loadSynthDrum(MIDI);
-};
-
-export default loadSoundfonts
