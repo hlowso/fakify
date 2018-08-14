890ed7b4ca27456d7c6b7a0566f30548df42da2b
hlowso <h.canderleigh@gmail.com>
2018-08-13 20:29:47 -0400

    another attempt

--- src/backend/index.ts
+++ src/backend/index.ts
@@ -1,8 +1,8 @@
 import express from "express";
 import path from "path";
 
-// import fs from "fs";
-// let files = fs.readdirSync('.');
+import fs from "fs";
+let files = fs.readdirSync('./build/static/js');
 // /Users/hlowso/Projects/precomp/precomp-frontend/
 
 const port = process.env.PORT || 5000;
@@ -11,7 +11,7 @@ const server = express();
 server.use(express.static('build'));
 
 server.get(['/', '/login', '/signup'], (req, res) => { 
-    console.log("HIT THE ENDPOINT");
+    console.log("HIT THE ENDPOINT", files);
     res.sendFile(path.join(__dirname, "/build/index.html")); 
 });
 
