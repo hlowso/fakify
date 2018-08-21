d4b90184337f2a1c63cf4a1bfd9e4bc013043a43
hlowso <h.canderleigh@gmail.com>
2018-08-20 22:34:19 -0400

    allowing options

--- src/backend/index.ts
+++ src/backend/index.ts
@@ -61,7 +61,7 @@ const exitHandler = (data: PreCompData, options: any, exitCode: number) => {
     server.use((req, res, next) => {
         res.header("Access-Control-Allow-Origin", "*");
         res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-        res.header("Access-Control-Allow-Methods", "POST, GET, PUT, PATCH, DELETE");
+        res.header("Access-Control-Allow-Methods", "POST, GET, PUT, PATCH, DELETE, OPTIONS");
         next();
     });
 
