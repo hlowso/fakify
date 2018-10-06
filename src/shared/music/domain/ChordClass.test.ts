07de342a629d931a3c5e4a8fa676f7a9d506cb75
hlowso <h.canderleigh@gmail.com>
2018-10-06 13:06:28 -0400

    added noteclasses test

--- src/shared/music/domain/ChordClass.test.ts
+++ src/shared/music/domain/ChordClass.test.ts
@@ -2,7 +2,7 @@ import * as Util from "../../Util";
 import * as MusicHelper from "../../music/MusicHelper";
 import { Chord } from "./ChordClass";
 import { ChordShape, NoteName, ChordName } from "../../types";
-// import { verifyNoteClasses } from "./Domain.test";
+import { Note } from "./Note";
 
 const testChordNames: ChordName[] = [ 
     [ "F", ChordShape.Min7$11 ], 
@@ -10,6 +10,35 @@ const testChordNames: ChordName[] = [
     [ "E", ChordShape.Dom9$5 ],
     [ "C#|Db", ChordShape.Maj7$11 ],
     [ "G", ChordShape.Dim7 ] 
+];
+
+interface IChordDatum {
+    chordName: ChordName;
+    reqNotes: NoteName[];
+    notes: NoteName[];
+}
+
+const chordData: IChordDatum[] = [
+    {
+        chordName: [ "A", ChordShape.Dim ],
+        reqNotes: [ "C", "D#|Eb" ],
+        notes: [ "A", "C", "D#|Eb" ]
+    },
+    {
+        chordName: [ "G#|Ab", ChordShape.Dom7$11 ],
+        reqNotes: [ "C", "F#|Gb", "D" ],
+        notes: [ "G#|Ab", "C", "D#|Eb", "F#|Gb", "D" ]       
+    },
+    {
+        chordName: [ "D", ChordShape.Min7b9 ],
+        reqNotes: [ "F", "C", "D#|Eb" ],
+        notes: [ "D", "F", "A", "C", "D#|Eb" ]        
+    },
+    {
+        chordName: [ "F#|Gb", ChordShape.Maj7$9 ],
+        reqNotes: [ "A#|Bb", "F", "A" ],
+        notes: [ "F#|Gb", "A#|Bb", "C#|Db", "F", "A" ]        
+    }
 ];
 
 test("with no input, getTonicPitch returns the absolute lowest tonic pitch", () => {
@@ -30,6 +59,11 @@ test("with no input, getFifthPitch returns the absolute lowest fifth pitch", () 
 
 test("constructor generates noteClasses with the right pitches", () => {
 
+    chordData.forEach(datum => {
+        let chord = new Chord(datum.chordName);
+        expect(noteClassesTest(chord, datum.notes)).toBeTruthy();
+    });
+
 });
 
 test("constructor generates notes array in which every value is defined", () => {
@@ -47,33 +81,13 @@ test("applyMutation gives the note classes of the objective scale the right pitc
 
 test("voice generates array containing all required pitches", () => {
 
-    interface IChordNotesPair {
-        chordName: ChordName;
-        notes: NoteName[];
-    }
-
-    let chordAndRequiredNotes: IChordNotesPair[] = [
-        {
-            chordName: [ "A", ChordShape.Dim ],
-            notes: [ "C", "D#|Eb" ]
-        },
-        {
-            chordName: [ "G#|Ab", ChordShape.Dom7$11 ],
-            notes: [ "C", "F#|Gb", "D" ]
-        },
-        {
-            chordName: [ "D", ChordShape.Min7b9 ],
-            notes: [ "F", "C", "D#|Eb" ]
-        }
-    ];
-
-    chordAndRequiredNotes.forEach(pair => {
-        let chord = new Chord(pair.chordName);
+    chordData.forEach(datum => {
+        let chord = new Chord(datum.chordName);
         let voicing: number[] = [];
 
         for (let i = 0; i < 10; i ++) {
             voicing = voicing.length > 0 ? chord.voice(60, voicing) : chord.voice(60);
-            expect(voicingPitchesTest(voicing, pair.notes)).toBeTruthy();
+            expect(voicingPitchesTest(voicing, datum.reqNotes)).toBeTruthy();
         }
     });
 
@@ -119,4 +133,21 @@ function voicingPitchesTest(voicing: number[], pitches: NoteName[]) {
     let voicingMod12 = voicing.map(pitch => Util.mod(pitch, 12));
 
     return !basePitches.some(pitch => voicingMod12.indexOf(pitch) === -1);
+}
+
+/**
+ * @param chord
+ * @param pitches
+ * @returns true if each name in pitches is represented by one of the note classes of the chords and vice versa
+ */
+function noteClassesTest(chord: Chord, pitches: NoteName[]) {
+
+    let basePitches = pitches.map(name => Chord.NOTE_NAMES.indexOf(name));
+
+    let noteFailure = (note: Note) => basePitches.indexOf(note.basePitch) === -1 || pitches.indexOf(note.name) === -1;
+
+    let correctChordNoteClasses = !chord.noteClasses.some(noteFailure);
+
+    return correctChordNoteClasses && pitches.length === chord.noteClasses.length;
+
 }
