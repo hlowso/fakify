42c70ee7e124bc93997ff0bc3f1d384ffd3fce58
hlowso <h.canderleigh@gmail.com>
2018-10-17 09:34:19 -0400

    uncommented test

--- src/shared/music/Chart.test.ts
+++ src/shared/music/Chart.test.ts
@@ -2,9 +2,10 @@ import Chart from "./Chart";
 import _251_bars from "../test-data/bars-251";
 import _4_chord_bars from "../test-data/bars-4-chords";
 import { chordStretches251InBbMajor } from "../test-data/chordStretches251InBbMajor";
-import { Feel, IChartBar } from "../types";
+import { Feel, IChartBar, IChordStretch } from "../types";
 
 const chart251InBbMajor = new Chart(() => {}, _251_bars as IChartBar[], "A#|Bb", [ 120, 4 ], Feel.Swing);
+const chart4Chords = new Chart(() => {}, _4_chord_bars as IChartBar[], "F#|Gb", [ 120, 4 ], Feel.Swing);
 
 test("valid tempo returns true when tempo is valid", () => {
 	expect(Chart.validTempo([120, 4])).toBe(true);
@@ -19,16 +20,14 @@ test("chordStretches are generated properly", () => {
 	expect(chordStretches).toEqual(chordStretches251InBbMajor);
 });
 
-// test("chordStretches have all integer durationInSubbeats values", () => {
-// 	let { chordStretches } = chart4Chords;
-// 	(chordStretches as IChordStretch[]).forEach(stretch => {
-// 		expect(Number.isInteger(stretch.durationInSubbeats as number)).toBeTruthy();
-// 	});
-// });
+test("chordStretches have all integer durationInSubbeats values", () => {
+	let { chordStretches } = chart4Chords;
+	(chordStretches as IChordStretch[]).forEach(stretch => {
+		expect(Number.isInteger(stretch.durationInSubbeats as number)).toBeTruthy();
+	});
+});
 
 test("different chord stretches are generated for chord segments within the same bar", () => {
-	const chart4Chords = new Chart(() => {}, _4_chord_bars as IChartBar[], "F#|Gb", [ 120, 4 ], Feel.Swing);
-
 	let { chordStretches } = chart4Chords;
 	expect(chordStretches).toHaveLength(4);
 });
